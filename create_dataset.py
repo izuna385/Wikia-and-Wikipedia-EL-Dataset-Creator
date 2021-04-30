d96901b36213b874b12c5bae66628c02131825cd
izuna385 <izuna385@gmail.com>
2021-04-30 14:31:34 +0900

    ignore redirects. TBD: consider them too for EL

--- create_dataset.py
+++ create_dataset.py
@@ -46,7 +46,8 @@ class Preprocessor:
                     annotations, sents = self._one_page_text_preprocessor(title=title, text=one_page_text)
                     sents = self._section_anchor_remover(sents)
                     entire_annotations += annotations
-                    doc_title2sents.update({title: sents})
+                    if sents != list(): # TODO: Treat redirects.
+                        doc_title2sents.update({title: sents})
                     debug_idx += 1
 
                     if debug_idx == 500:
